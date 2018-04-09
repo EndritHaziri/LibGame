6b5ffb8436c000be3ea3b96bdc4968277cf17c0d
EndritHaziri <endrit_haziri@hotmail.com>
2018-04-09 14:45:43 +0200

    Initial commit

---
+++ app/src/main/java/com/example/endrithaziri/libgame/AddGame.java
@@ -0,0 +1,43 @@
+package com.example.endrithaziri.libgame;
+
+/**
+ * Created by Endrit Haziri on 04.04.2018.
+ */
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class AddGame extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_cancel:
+                    mTextMessage.setText(R.string.title_cancel);
+                    return true;
+
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/com/example/endrithaziri/libgame/Home.java
@@ -0,0 +1,61 @@
+package com.example.endrithaziri.libgame;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+public class Home extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private ImageButton imageButton;
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_add:
+                    mTextMessage.setText(R.string.title_add);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        imageButton = findViewById(R.id.imageGame1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gamepage = new Intent (Home.this,GamePage.class);
+                Home.this.startActivity(gamepage);
+
+            }
+        });
+
+
+
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+  //      BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+    //    navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}

33f9b64967256c38a792959f0c2334d26f76e426
Guordek <30309206+Guordek@users.noreply.github.com>
2018-04-17 11:45:23 +0200

    Add Strings

--- app/src/main/java/com/example/endrithaziri/libgame/AddGame.java
+++ app/src/main/java/com/example/endrithaziri/libgame/AddGame.java
@@ -4,42 +4,17 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.annotation.NonNull;
-import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 public class AddGame extends AppCompatActivity {
 
     private static final int PICK_IMAGE = 100;
-    private TextView mTextMessage;
-    private Button button;
-    private ImageView imageView;
-    private Uri imageUri;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
+    Button button;
+    ImageView imageView;
+    Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,13 +28,8 @@ public class AddGame extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 openGallery();
-                System.out.println("click");
             }
         });
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
     private void openGallery() {

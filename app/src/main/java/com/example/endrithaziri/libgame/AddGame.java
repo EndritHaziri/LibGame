1212bce3b2cd244b5378541f4e5a697533c4dac7
EndritHaziri <endrit_haziri@hotmail.com>
2018-04-22 18:19:13 +0200

    Changement de la classe AddGame pour la sauvegarde des images

--- app/src/main/java/com/example/endrithaziri/libgame/AddGame.java
+++ app/src/main/java/com/example/endrithaziri/libgame/AddGame.java
@@ -1,29 +1,40 @@
 package com.example.endrithaziri.libgame;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.design.internal.BottomNavigationItemView;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 import entity.Game;
 import view_model.GameViewModel;
 
 public class AddGame extends AppCompatActivity {
 
-    private static final int PICK_IMAGE = 100;
+    private static final int PICK_IMAGE_REQUEST = 100;
     public static final String EXTRA_REPLY = "com.example.android.wordlistsql.REPLY";
 
     Button buttonImg;
     Button buttonAddDev;
     Button buttonAddPub;
     BottomNavigationItemView buttonAddGame;
-    ImageView imageView;
+    ImageView image;
     Uri imageUri;
     private GameViewModel gameViewModel;
 
@@ -44,13 +55,21 @@ public class AddGame extends AppCompatActivity {
         buttonAddPub = (Button) findViewById(R.id.buttonAddPub);
         buttonAddGame = (BottomNavigationItemView) findViewById(R.id.navigation_add);
 
-        imageView = (ImageView) findViewById(R.id.imageViewAddGame);
+        image = (ImageView) findViewById(R.id.imageViewAddGame);
+
+        SharedPreferences myPrefrence = getPreferences(MODE_PRIVATE);
+        String imageS = myPrefrence.getString("imagePreferance", "");
+        Bitmap imageB;
+        if(!imageS.equals("")) {
+            imageB = decodeToBase64(imageS);
+            image.setImageBitmap(imageB);
+        }
 
         // Listener button image
         buttonImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGallery();
+                openGallery(view);
             }
         });
 
@@ -82,7 +101,6 @@ public class AddGame extends AppCompatActivity {
         });
 
     }
-
     private void saveData() {
         Intent replyIntent = new Intent();
         if (TextUtils.isEmpty("test")) {
@@ -94,19 +112,60 @@ public class AddGame extends AppCompatActivity {
         }
         finish();
     }
-
-    private void openGallery() {
-        Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(gallery, PICK_IMAGE);
+    public void openGallery(View v){
+        Intent intent = new Intent();
+// Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+// Always show the chooser (if there are multiple options available)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
     }
 
     @Override
-    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE) {
-            imageUri = data.getData();
-            imageView.setImageURI(imageUri);
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            InputStream stream;
+            try {
+                Toast.makeText(AddGame.this, "Image saved", Toast.LENGTH_SHORT).show();
+                stream = getContentResolver().openInputStream(data.getData());
+                Bitmap realImage = BitmapFactory.decodeStream(stream);
+                image.setImageBitmap(realImage);
+
+
+                SharedPreferences myPrefrence = getPreferences(MODE_PRIVATE);
+                SharedPreferences.Editor editor = myPrefrence.edit();
+                editor.putString("imagePreferance", encodeToBase64(realImage));
+
+                editor.commit();
+            }
+            catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         }
     }
+
+    public static String encodeToBase64(Bitmap image) {
+        Bitmap immage = image;
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        immage.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        byte[] b = baos.toByteArray();
+        String imageEncoded = Base64.encodeToString(b, Base64.DEFAULT);
+
+        Log.d("Image Log:", imageEncoded);
+        return imageEncoded;
+    }
+
+    public static Bitmap decodeToBase64(String input) {
+        byte[] decodedByte = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
+    }
+
 
 }
